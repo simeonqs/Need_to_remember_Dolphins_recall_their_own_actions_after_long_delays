5f030a7246d26a0251084a4529df3da6e072bd06
Simeon Q. Smeele <simeonqs@hotmial.com>
2024-07-31 09:59:17 +0200

    Updating 10_plot_figure_1.R. Time on natural scale. Adding data for no delay as baseline together with model estimate.

--- analysis/code/10_plot_figure_1.R
+++ analysis/code/10_plot_figure_1.R
@@ -18,39 +18,50 @@ rm(list = ls())
 set.seed(1)
 
 # Paths
-path_source_data = 'analysis/code/06_retrospective_delay_load_data.R'
+path_source_data_delay = 'analysis/code/06_retrospective_delay_load_data.R'
 path_m_retrospective_delay = 'analysis/results/m_retrospective_delay.RData'
+path_source_data_no_delay = 
+  'analysis/code/04_retrospective_no_delay_load_data.R'
+path_m_retrospective_no_delay = 
+  'analysis/results/m_retrospective_no_delay.RData'
 path_pdf = 'analysis/results/figure_1.pdf'
 
 # Load data
-source(path_source_data)
+source(path_source_data_delay)
+source(path_source_data_no_delay)
 load(path_m_retrospective_delay)
 
 # Plot per species
 pdf(path_pdf, 3.5, 3)
 par(mar = c(3.5, 2, 1, 0.5))
-plot(clean_dat$log_time + rnorm(nrow(clean_dat), 0, 0.02), 
+plot(2^clean_dat$log_time + rnorm(nrow(clean_dat), 0, 0.02), 
      clean_dat$response + rnorm(nrow(clean_dat), 0, 0.02),
-     pch = 16, col = alpha('grey', 0.5),
-     xlim = c(log2(3.3), log2(23)),
+     pch = 16, col = 'grey',
+     xlim = c(0, 23),
      xaxt = 'n', yaxt = 'n', xlab = '', ylab = '', 
      main = '')
 mtext('Delay [seconds]', 1, 2.3)
-axis(1, log2(c(2, 4, 8, 16)), c(2, 4, 8, 16))
+axis(1, c(0, 5, 10, 15, 20, 25))
 axis(2, c(0, 1), c('Incorrect', 'Correct'))
 times = seq(min(clean_dat$log_time), max(clean_dat$log_time), 0.1)
 for(i in sample(length(post$a_bar), 20)) 
-  lines(times,
+  lines(2^times,
         inv_logit(post$a_bar[i] - post$b_bar[i] * times),
-        lty = 1, lwd = 3, col = alpha('grey', 0.5))
+        lty = 1, lwd = 3, col = 'grey')
 fitted = vapply(times, function(time) 
   vapply(seq_len(length(post$a_bar)), function(i)
     post$a_bar[i] - post$b_bar[i] * time, numeric(1)) |> mean(),
   numeric(1))
-lines(times, inv_logit(fitted), lwd = 5, col = alpha('black', 0.7))
-lines(times, rep(1/4, length(times)), lty = 5, 
-      lwd = 3, col = alpha('black', 0.7))
-text(min(times), 1/4 + 0.1, 'Chance level', adj = 0)
+lines(2^times, inv_logit(fitted), lwd = 5)
+lines(2^times, rep(1/4, length(times)), lty = 5, lwd = 3)
+text(2^min(times), 1/4 + 0.1, 'Chance level', adj = 0)
+load(path_m_retrospective_no_delay)
+points(0.5 + rnorm(nrow(clean_dat_retrospective_no_delay), 0, 0.02), 
+       clean_dat_retrospective_no_delay$response + 
+         rnorm(nrow(clean_dat_retrospective_no_delay), 0, 0.02),
+       pch = 16, col = 'grey')
+points(0.5, post$a_bar |> mean() |> inv_logit(), pch = 16, cex = 1.5)
+lines(rep(0.5, 2), post$a_bar |> PI() |> inv_logit(), lwd = 5)
 dev.off()
 
 # Message 
